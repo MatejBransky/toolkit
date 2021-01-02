40bd4790b8638608969284c49b97f1c526850f44
Matej Bransky <matej.bransky@ysoft.com>
2021-01-02 13:29:24 +0100

    update eslint-config for vanillajs projects

--- packages/eslint-config/vanilla.js
+++ packages/eslint-config/vanilla.js
@@ -1,5 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: ['prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  rules: {
+    '@typescript-eslint/no-redeclare': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'prettier/prettier': 'warn',
+  },
   ignorePatterns: 'dist',
 };
